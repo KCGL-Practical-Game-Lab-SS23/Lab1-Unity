85112f624df131e4aef1c24d440b79ecf85df80b
Michele Fanelli <mickyfanelli@hotmail.it>
2023-03-17 10:55:28 +0100

    AI task

--- Lab1-Unity/Assets/Scripts/EnemyController.cs
+++ Lab1-Unity/Assets/Scripts/EnemyController.cs
@@ -1,33 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyController : MonoBehaviour
 {
-    private enum WhereToGo { AwayFromPlayer, RandomDirection, RandomDestination, ToFarthestDestination, ToClosestObstacle };
+    private enum ChaseBehaviour { ChasePlayer, IsChasedByPlayer };
+    private enum IdleBehaviour { StayStill, MoveRandomly };
+    private enum WhereToRun { ToRandomDestination, ToFarthestDestination, ToClosestObstacle };
 
     private Transform t;
     private Rigidbody2D rb;
+    private Seeker seeker;
     //private Animator a;
     //private SpriteRenderer sr;
-
-    private Vector2 direction;
-    private Vector3 zeroVelocity = Vector3.zero;
-    private Transform playerTransform;
-    private bool playerIsClose = false;
 
     [Header("Movement")]
     [SerializeField] private bool canMove = true;
     [SerializeField] private float movementSpeed = 10f;
     [SerializeField] private float movementSmoothing = 0.5f;
 
+    private Vector2 direction;
+    private Vector3 zeroVelocity = Vector3.zero;
+    private Transform playerTransform;
+
     [Header("Behaviour")]
-    [SerializeField] private bool moveOnlyIfPlayerIsClose = true;
-    [SerializeField] [Range(0, 10f)] private float triggerPlayerDistance = 2f;
-    [SerializeField] private WhereToGo whereToGo = WhereToGo.AwayFromPlayer;
-    //[SerializeField] private bool chasePlayer = false;
-    [SerializeField] private bool changeSpeedWhenCloseToPlayer = false;
-    [SerializeField] [Range(-3f, 3f)] private float changedSpeedMultiplier = 0f;
+    [SerializeField] private ChaseBehaviour chaseOrChased = ChaseBehaviour.ChasePlayer;
+    [SerializeField] private WhereToRun whereToRun = WhereToRun.ToFarthestDestination;
+    [SerializeField] private bool triggerIfClose = true;
+    [SerializeField] [Range(0, 10f)] private float triggerDistance = 2f;
+    [SerializeField] private IdleBehaviour idleBehaviour = IdleBehaviour.StayStill;
+    [SerializeField] private bool panicSpeed = false;
+    [SerializeField] [Range(-3f, 3f)] private float panicSpeedMultiplier = 0f;
+
+    private float nextWaypointDistance = 3f;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+    private Path path;
+    private Transform target;
+    
+    
 
 
 
@@ -35,59 +47,59 @@ public class EnemyController : MonoBehaviour
     {
         t = GetComponent<Transform>();
         rb = GetComponent<Rigidbody2D>();
+        seeker = GetComponent<Seeker>();
         //a = GetComponent<Animator>();
         //sr = GetComponent<SpriteRenderer>();
 
+
         playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+
+        if (chaseOrChased == ChaseBehaviour.ChasePlayer)
+            target = playerTransform;
+
+        ChooseDestination();
+        InvokeRepeating("UpdatePath", 0f, .5f);
     }
 
     private void Update()
     {
-        switch (whereToGo)
-        {
-            case WhereToGo.AwayFromPlayer:
-                direction = t.position - playerTransform.position;
-                break;
-            case WhereToGo.RandomDirection:
-                direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
-                break;
-            case WhereToGo.RandomDestination:
-                //To Do
-                break;
-            case WhereToGo.ToFarthestDestination:
-                //To Do
-                break;
-            case WhereToGo.ToClosestObstacle:
-                //To Do
-                break;
-        }
-
-        playerIsClose = (playerTransform.position - t.transform.position).magnitude <= triggerPlayerDistance;
-                
-        if (moveOnlyIfPlayerIsClose && !playerIsClose)
-            direction = Vector2.zero;
+        ChooseDestination();
     }
 
     private void FixedUpdate()
     {
-        if (canMove)
+        if (path == null)
+            return;
+
+        if (currentWaypoint >= path.vectorPath.Count)
         {
-            var velocity = direction.normalized * movementSpeed;
-
-            //a.SetFloat("Speed", velocity.magnitude);
-
-            /*if (velocity.x > 0)
-                sr.flipX = true;
-            if (velocity.x < 0)
-                sr.flipX = false;*/
-
-            if (changeSpeedWhenCloseToPlayer)
-                velocity *= changedSpeedMultiplier;
-
-            rb.velocity = Vector3.SmoothDamp(rb.velocity, velocity * movementSpeed * Time.fixedDeltaTime, ref zeroVelocity, movementSmoothing);
+            reachedEndOfPath = true;
+            return;
         }
         else
-            rb.velocity = Vector2.zero;
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * movementSpeed * Time.fixedDeltaTime;
+
+        rb.AddForce(force);
+
+        /*if (canMove)
+        {
+            var velocity = direction * movementSpeed;
+
+            if (panicSpeed)
+                velocity *= panicSpeedMultiplier;
+
+            rb.velocity = Vector3.SmoothDamp(rb.velocity, velocity * movementSpeed * Time.fixedDeltaTime, ref zeroVelocity, movementSmoothing);
+        }*/
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if (distance < nextWaypointDistance)
+            currentWaypoint++;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -98,5 +110,47 @@ public class EnemyController : MonoBehaviour
             Destroy(this.gameObject);
         }
 
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }            
+    }
+
+    private void UpdatePath()
+    {
+        if (seeker.IsDone())
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+    }
+
+    private void ChooseDestination()
+    {
+        if (chaseOrChased == ChaseBehaviour.ChasePlayer)
+            target = playerTransform;
+        else
+            switch (whereToRun)
+            {
+                case WhereToRun.ToRandomDestination:
+                    break;
+                case WhereToRun.ToFarthestDestination:
+                    break;
+                case WhereToRun.ToClosestObstacle:
+                    break;
+            }
+
+        if (triggerIfClose && (playerTransform.position - t.position).magnitude > triggerDistance)
+            switch (idleBehaviour)
+            {
+                case IdleBehaviour.StayStill:
+                    target = t;
+                    break;
+                case IdleBehaviour.MoveRandomly:
+                    //Choose random destination
+                    break;
+            }
     }
 }
